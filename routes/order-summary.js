9ada22f6be7dd103c325482103a9c597216a3857
BoxinCao <ngxmex@gmail.com>
2025-05-06 19:08:20 -0400

    updated

--- routes/order-summary.js
+++ routes/order-summary.js
@@ -6,13 +6,13 @@ router.get('/:id', (req, res) => {
   const orderId = req.params.id;
 
   db.query(`
-    SELECT orders.id AS order_number, 
+    SELECT id, 
            total_price, 
            order_time, 
            ready_time, 
            (ready_time - order_time) AS wait_time
     FROM orders
-    WHERE orders.id = $1;
+    WHERE id = $1;
   `, [orderId])
     .then(result => {
 
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
       const status = "in progress";
 
       res.render('order_summary', {
-        orderNumber: order.order_number,
+        orderNumber: order.id,
         totalPrice: order.total_price,
         status,
         waitTime: order.wait_time
